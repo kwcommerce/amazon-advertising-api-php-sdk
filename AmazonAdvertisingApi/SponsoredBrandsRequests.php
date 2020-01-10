7433da283b31003dd4f5c9fcac92b927f5841fa3
Rodion Goncov <goncovwork@gmail.com>
2020-01-10 12:07:27 +0200

    added wrappers of `/sb/adGroups` and `/sb/adGroups/{adGroupId}`

---
+++ AmazonAdvertisingApi/SponsoredBrandsRequests.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace AmazonAdvertisingApi;
+
+
+trait SponsoredBrandsRequests
+{
+    /**
+     * @see https://advertising.amazon.com/API/docs/en-us/sponsored-brands/3-0/openapi#/Campaigns/listAdGroups
+     * @param null $data
+     * @return array
+     * @throws \Exception
+     */
+    public function listSponsoredBrandAdGroups($data = null): array
+    {
+        return $this->_operation("sb/adGroups", $data);
+    }
+
+    /**
+     * @see https://advertising.amazon.com/API/docs/en-us/sponsored-brands/3-0/openapi#/Campaigns/getAdGroups
+     * @param int $adGroupId
+     * @return array
+     * @throws \Exception
+     */
+    public function getSponsoredBrandCampaign(int $adGroupId): array
+    {
+        return $this->_operation("sb/adGroups/{$adGroupId}");
+    }
+}
