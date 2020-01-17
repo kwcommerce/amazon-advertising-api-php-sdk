592c248ff9450f86acb0574f6cd5312515af705b
Rodion Goncov <goncovwork@gmail.com>
2020-01-17 09:16:17 +0200

    added routes of SB keywords

--- AmazonAdvertisingApi/SponsoredBrandsRequests.php
+++ AmazonAdvertisingApi/SponsoredBrandsRequests.php
@@ -27,4 +27,63 @@ trait SponsoredBrandsRequests
     {
         return $this->_operation("sb/adGroups/{$adGroupId}");
     }
+
+    /**
+     * Gets an array of keywords, filtered by optional criteria.
+     * @see https://advertising.amazon.com/API/docs/en-us/sponsored-brands/3-0/openapi#/Keywords/listKeywords
+     * @param null $data
+     * @return array
+     * @throws \Exception
+     */
+    public function listSponsoredBrandKeywords($data = null): array
+    {
+        return $this->_operation("sb/keywords", $data);
+    }
+
+    /**
+     * Updates one or more keywords.
+     * @see https://advertising.amazon.com/API/docs/en-us/sponsored-brands/3-0/openapi#/Keywords/updateKeywords
+     * @param array $data
+     * @return array
+     * @throws \Exception
+     */
+    public function updateSponsoredBrandKeywords(array $data): array
+    {
+        return $this->_operation("sb/keywords", $data, "PUT");
+    }
+
+    /**
+     * @see https://advertising.amazon.com/API/docs/en-us/sponsored-brands/3-0/openapi#/Keywords/createKeywords
+     * @param array $data
+     * @return array
+     * @throws \Exception
+     */
+    public function createSponsoredBrandKeywords(array $data): array
+    {
+        return $this->_operation("sb/keywords", $data, "POST");
+    }
+
+    /**
+     * Gets a keyword specified by identifier.
+     * @see https://advertising.amazon.com/API/docs/en-us/sponsored-brands/3-0/openapi#/Keywords/getKeyword
+     * @param int $keywordId
+     * @return array
+     * @throws \Exception
+     */
+    public function getSponsoredBrandKeyword(int $keywordId): array
+    {
+        return $this->_operation("sb/keywords/{$keywordId}");
+    }
+
+    /**
+     * Archives a keyword specified by identifier.
+     * @see https://advertising.amazon.com/API/docs/en-us/sponsored-brands/3-0/openapi#/Keywords/archiveKeyword
+     * @param int $keywordId
+     * @return array
+     * @throws \Exception
+     */
+    public function archiveSponsoredBrandKeyword(int $keywordId): array
+    {
+        return $this->_operation("sb/keywords/{$keywordId}", "DELETE");
+    }
 }
