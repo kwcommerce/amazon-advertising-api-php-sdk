fba00df82dda91ac4f41fe5804a99e1082363aa7
Sergey Kubrey <kubrey.work@gmail.com>
2020-08-12 19:23:19 +0300

    fix sb routes

--- AmazonAdvertisingApi/Client.php
+++ AmazonAdvertisingApi/Client.php
@@ -1299,7 +1299,7 @@ class Client
         if ($reportType === 'sponsoredProducts') {
             return 'sp';
         } elseif ($reportType === 'sponsoredBrands') {
-            return 'sb';
+            return 'hsa';
         } elseif ($reportType === 'sponsoredDisplay') {
             return 'sd';
         } else {
